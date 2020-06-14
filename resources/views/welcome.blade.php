876ed25125681800b6582394c3322811b96d6ef0
Neel278 <neelthakkar2001@gmail.com>
2020-06-14 16:00:57 +0530

    master page deleted and welcome page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Live wire</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <livewire:styles>
-        <script src="{{ asset('js/app.js') }}" data-turbolinks-suppress-warning></script>
+@section('content')
 
-</head>
+@livewire('index')
 
-<body>
-    @include('header')
-
-    @include('layouts.app')
-
-    @livewire('index')
-
-    @livewire('footer')
-
-
-    <livewire:scripts>
-</body>
-
-</html>
+@endsection
